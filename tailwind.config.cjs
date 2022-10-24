2a342aa95a9386a3554de0547645180e427b0b46
Pohl <m.pohl@drk-rostock.de>
2022-10-24 12:19:07 +0200

    added fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
+    fontFamily: {
+      primary: ["Koulen", "cursive"],
+      secondary: ["Convergence", "sans-serif"],
+    },
   },
   plugins: [],
 };
