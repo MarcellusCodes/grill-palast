4df3353422fccbf0928269c7c95579ec9d53274f
Pohl <m.pohl@drk-rostock.de>
2022-10-24 12:51:16 +0200

    added theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#343740",
+        secondary: "#883772",
+        tertiary: "#C04C71",
+        quaternary: "#FCBE67",
+      },
+    },
     fontFamily: {
       primary: ["Koulen", "cursive"],
       secondary: ["Convergence", "sans-serif"],
